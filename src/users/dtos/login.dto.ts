dac391fcf5e5f1ea46e944168a8b3c07dc22d143
Aleksander Yurkovsky <godlikedesigner@gmail.com>
2021-02-05 23:47:17 +0500

    Refactor dto and core entity.

--- src/users/dtos/login.dto.ts
+++ src/users/dtos/login.dto.ts
@@ -1,12 +1,12 @@
 import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
 import { User } from '../entities/user.entity';
-import { MutationOutput } from './../../common/dto/output.dto';
+import { CoreOutput } from './../../common/dto/output.dto';
 
 @InputType()
 export class LoginInput extends PickType(User, ['email', 'password']) {};
 
 @ObjectType()
-export class LoginOutput extends MutationOutput {
+export class LoginOutput extends CoreOutput {
   @Field(type => String, {nullable: true})
   token?: string;
 }
